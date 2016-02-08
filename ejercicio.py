8248ebd7e227121116ac5fb0626ff617ecf4c08e
Fernando Aguilar Santos <fsantos@zeta40.aulas.gsyc.es>
2016-02-08 11:50:34 +0100

    Ejerc de dicc hecho

--- ejercicio.py
+++ ejercicio.py
@@ -1,13 +1,23 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-# hola, hola
+#Fernando Aguilar Santos
 
 fd = open('/etc/passwd', 'r')
 
-lineas = fd.readlines()
+Dicc = {}							#Declaro el diccionario
+lineas = fd.readlines()			
 fd.close()
 
 for linea in lineas:
-    elementos = linea.split(':')
-    print elementos[0], elementos[-1][:-1]
+	elementos = linea.split(':')
+	Dicc[elementos[0]]= elementos[-1][:-1] 			#Guardo cada key y value en el diccionario(1 a 1)
+
+try:
+
+	print Dicc["root"]
+	print Dicc["imaginario"]
+
+except :			#elevo la excepción para el caso de que no encuentro la key
+
+	print "key no encontrada"
